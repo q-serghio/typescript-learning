8cd5655ddd20aee237fafef3cec85dd022759379
Serghio <quantumnetworks.ms@gmail.com>
2025-04-28 23:59:44 +0200

    generics part1

---
+++ src/generics.ts
@@ -0,0 +1,38 @@
+// In TypeScript, you can declare an array using two syntaxes:
+
+// let array1: string[] = ['Apple', 'Banana', 'Mango'];
+// let array2: number[] = [1, 2, 3];
+// let array3: boolean[] = [true, false, true];
+
+let array1: Array<string> = ["Apple", "Banana", "Mango"];
+let array2: Array<number> = [1, 2, 3];
+let array3: Array<boolean> = [true, false, true];
+
+//
+function createString(arg: string): string {
+  return arg;
+}
+function createNumber(arg: number): number {
+  return arg;
+}
+
+// Define a generic function
+function genericFunction<T>(arg: T): T {
+  return arg;
+}
+
+const someStringValue = genericFunction<string>("Hello World");
+const someNumberValue = genericFunction<number>(2);
+
+// Define a generic interface
+interface GenericInterface<T> {
+  value: T;
+  getValue: () => T;
+}
+
+const genericString: GenericInterface<string> = {
+  value: "Hello World",
+  getValue() {
+    return this.value;
+  },
+};
